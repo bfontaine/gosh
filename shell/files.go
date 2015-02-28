4f8f75d0303072526d06de4b5612354be5a9911e
Baptiste Fontaine <batifon@yahoo.fr>
2015-02-28 12:04:16 +0100

    expandPath: absolute paths support

--- shell/files.go
+++ shell/files.go
@@ -2,8 +2,20 @@ package shell
 
 import (
 	"os"
+	"path/filepath"
 )
 
 func expandPath(path string) string {
-	return os.ExpandEnv(path)
+	// replace env variables
+	expandedPath := os.ExpandEnv(path)
+
+	// get an absolute path, ignoring errors
+	if absPath, err := filepath.Abs(expandedPath); err == nil {
+		expandedPath = absPath
+	}
+
+	// cleanup
+	expandedPath = filepath.Clean(expandedPath)
+
+	return expandedPath
 }
--- shell/files_test.go
+++ shell/files_test.go
@@ -2,6 +2,8 @@ package shell
 
 import (
 	"github.com/franela/goblin"
+	"os"
+	"runtime"
 	"testing"
 )
 
@@ -9,8 +11,48 @@ func Test(t *testing.T) {
 	g := goblin.Goblin(t)
 
 	g.Describe("expandPath", func() {
+
+		tmp := os.TempDir()
+
+		// On OS X, /var is a symlink to /private/var and expandPath doesn't
+		// follow symlinks.
+		if runtime.GOOS == "darwin" {
+			tmp = "/private" + tmp
+		}
+
+		g.BeforeEach(func() {
+			os.Setenv("foo", "bar")
+			os.Setenv("parent", "..")
+			os.Chdir(tmp)
+		})
+
+		g.AfterEach(func() {
+			os.Unsetenv("foo")
+			os.Unsetenv("parent")
+		})
+
 		g.It("Should leave absolute paths as is", func() {
 			g.Assert(expandPath("/foo/bar")).Equal("/foo/bar")
+		})
+
+		g.It("Should expand $env variables", func() {
+			g.Assert(expandPath("/foo/$foo")).Equal("/foo/bar")
+		})
+
+		g.It("Should expand ${env} variables", func() {
+			g.Assert(expandPath("/foo/x${foo}x")).Equal("/foo/xbarx")
+		})
+
+		g.It("Should return absolute paths", func() {
+			g.Assert(expandPath("./bar")).Equal(tmp + "bar")
+		})
+
+		g.It("Should interpret $env vars before absolut-ing paths", func() {
+			g.Assert(expandPath("./foo/$parent/bar")).Equal(tmp + "bar")
+		})
+
+		g.It("Should cleanup paths", func() {
+			g.Assert(expandPath("/./././foo/.././qux")).Equal("/qux")
 		})
 	})
 }
